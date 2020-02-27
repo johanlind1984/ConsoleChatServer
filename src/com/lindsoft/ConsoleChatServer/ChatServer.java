77a98592fe00a6201c101c5062a3926a3160f43b
johanlind1984 <54557482+johanlind1984@users.noreply.github.com>
2020-02-27 09:59:07 +0100

    removed obsolete imports

--- src/com/lindsoft/ConsoleChatServer/ChatServer.java
+++ src/com/lindsoft/ConsoleChatServer/ChatServer.java
@@ -1,9 +1,6 @@
 package com.lindsoft.ConsoleChatServer;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashSet;
